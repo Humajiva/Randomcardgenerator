e53cd9756a22719844906c96cd680772595fcf5e
Humajiva <humajiva123@gmail.com>
2022-01-20 02:39:15 +0000

    completed project

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomNmber = Math.floor(Math.random() * 12);
+  let randomSuit = Math.floor(Math.random() * 4);
+
+  let numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  let suits = ["&#9824", "&#9827", "&#9829", "&#9830"];
+
+  document.querySelector(".suit").innerHTML = suits[randomSuit];
+  document.querySelector(".number").innerHTML = numbers[randomNmber];
+  document.querySelector(".upside-down-suit").innerHTML = suits[randomSuit];
+
+  if (suits[randomSuit] == "&#9829" || suits[randomSuit] == "&#9830") {
+    document.querySelector(".suit").style.color = "red";
+    document.querySelector(".number").style.color = "red";
+    document.querySelector(".upside-down-suit").style.color = "red";
+  }
 };
